28fda7ada6b8d2c18ab71a018450f9f09fb705c7
asetiadi001 <asetiadi001@e.ntu.edu.sg>
2014-10-01 06:35:23 +0800

    fix protocol btwn android and arduino.

--- main.py
+++ main.py
@@ -39,58 +39,62 @@ class Main:
 			time.sleep (delay)
 
 	def btWrite (self, delay, android, serialq):
+		
 		stop_flag = 0
 		while stop_flag == 0:
 			time.sleep (delay)
+			#print "btWrite awake - not"
 			if len(serialq) >0:
 				msg = serialq.popleft()
-				print "IP queue length after pop: " , len(serialq)
+				print "Writing to android: ", msg
 				android.write(msg)
-				print "%s: %s --msg: %s" % ("btWrite", time.ctime(time.time()), msg)
-
+				print "Serial queue length after pop: " , len(serialq)
 
 	def btRead (self, delay, android, btq):
 		stop_flag = 0
 		while stop_flag == 0:
+			print 'btRead in blocking mode'
 			msg = android.read()
 			if(msg!=''):
+				print "From android: %s" % (msg)
 				btq.append(msg)
 				print "BT queue length after append: ", len(btq)
-				print "%s: %s--msg: %s" % ("btRead", time.ctime(time.time()),msg )
 				time.sleep (delay)
 
 	def serialWrite(self, delay, arduino, btq):
 		stop_flag = 0
 		while stop_flag == 0:
 			time.sleep(delay)
+			#print "serialWrite awake - not"
 			if len(btq) > 0:
 				msg = btq.popleft()
-				print "Enter serialWrite, writing %s to arduino" %(msg)
+				print "Writing to arduino: %s" %(msg)
 				arduino.write(msg)
-				print "BT queue length after append: ", len(btq)
-				print "%s: %s--msg: %s" % ("serialRead", time.ctime(time.time()),msg )
+				print "BT queue length after pop: ", len(btq)
+				#print "%s: %s--msg: %s" % ("serialRead", time.ctime(time.time()),msg )
 	
 	def serialRead(self, delay, arduino, serialq):
 		stop_flag = 0
 		while stop_flag == 0:
+			print "serialRead in blocking mode"
 			#if arduino.read() !=None: #check for empty string/char when reading.
 			msg = arduino.read()
 			#append the msg to both bluetooth queue and ip queue
 			if(msg!=''):
 				serialq.append(msg)
 				#ipq.append(msg)
+				print "From arduino: ",msg
 				print "Serial queue length after append: ", len(serialq)
 				#print "IP queue length after append: ", len(ipq)
-				print "%s: %s--msg: %s" % ("serialRead", time.ctime(time.time()),msg )
 			time.sleep(delay)
 
 	def startServices(self):
 		ready1=[False]
 		ready2=[False]
 		ready3=[False]
-		thread.start_new_thread(self.android.startBTService, (ready1))
-		thread.start_new_thread(self.pc.startIPService, (ready2))
-		thread.start_new_thread(self.arduino.startSerialService, (ready3))
+		thread.start_new_thread(self.android.startBTService, (ready1,))
+		thread.start_new_thread(self.pc.startIPService, (ready2,))
+		thread.start_new_thread(self.arduino.startSerialService, (ready3,))
 		while True:
 			if ready1[0]!=True or ready2[0]!=True or ready3[0]!=True:
 				pass
