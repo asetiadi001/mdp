27b870333ba3c31844b0c932f8efab18fc6de807
asetiadi001 <asetiadi001@e.ntu.edu.sg>
2014-09-30 13:12:27 +0800

    update main.py to be complete

--- main.py
+++ main.py
@@ -16,69 +16,101 @@ class Main:
 		self.btq = deque([])
 		self.serialq = deque([])
 
-	def ipWrite (self, delay, pc, btq):
-		stop_flag = 0
-		while stop_flag == 0:
-			time.sleep (delay)
-			if len(btq) >0:
-				msg = btq.popleft()
-				print "BT queue length after pop: " , len(btq)
-				pc.write(msg)
-				print "%s: %s --msg: %s" % ("ipWrite", time.ctime(time.time()), msg)
-
-
-	def ipRead (self, delay, pc, ipq):
-		stop_flag = 0
-		while stop_flag == 0:
-			time.sleep (delay)
-			msg = pc.read()
-			if msg !='':
-				ipq.append(msg)
-				print "IP queue length after append: ", len(ipq)
-				print "%s: %s--msg: %s" % ("ipRead", time.ctime(time.time()),msg )
-
-	def btWrite (self, delay, android, ipq):
+	def ipWrite (self, delay, pc, ipq):
 		stop_flag = 0
 		while stop_flag == 0:
 			time.sleep (delay)
 			if len(ipq) >0:
 				msg = ipq.popleft()
-				print "IP queue length after pop: " , len(ipq)
-				android.write(msg)
-				print "%s: %s --msg: %s" % ("btWrite", time.ctime(time.time()), msg)
+				print "WiFi queue length after pop: " , len(btq)
+				pc.write(msg)
+				print "%s: %s --msg: %s" % ("ipWrite", time.ctime(time.time()), msg)
 
-	def btRead (self, delay, android, btq):
+	def ipRead (self, delay, pc, btq, serialq):
+		stop_flag = 0
+		while stop_flag == 0:
+			msg = pc.read()
+			if(msg!=''):
+				btq.append(msg)
+				serialq.append(msg)
+				print "IP queue length after append: ", len(ipq)
+				print "%s: %s--msg: %s" % ("ipRead", time.ctime(time.time()),msg )
+			time.sleep (delay)
+
+	def btWrite (self, delay, android, btq):
 		stop_flag = 0
 		while stop_flag == 0:
 			time.sleep (delay)
-			msg = android.read()
-			if msg !='':
-				btq.append(msg)
-				print "BT queue length after append: ", len(btq)
-				print "%s: %s--msg: %s" % ("btRead", time.ctime(time.time()),msg )
+			if len(btq) >0:
+				msg = btq.popleft()
+				print "IP queue length after pop: " , len(btq)
+				android.write(msg)
+				print "%s: %s --msg: %s" % ("btWrite", time.ctime(time.time()), msg)
 
-	def serialWrite(self, delay):
+	#will not be used
+	def btRead (self, delay, android, ipq):
 		stop_flag = 0
+		while stop_flag == 0:
+			msg = android.read()
+			if(msg!=''):
+				ipq.append(msg)
+				print "BT queue length after append: ", len(ipq)
+				print "%s: %s--msg: %s" % ("btRead", time.ctime(time.time()),msg )
+			time.sleep (delay)
 
+	def serialWrite(self, delay, arduino, serialq):
+		stop_flag = 0
+		while stop_flag == 0:
+			time.sleep(delay)
+			if len(serialq) > 0:
+				msg = serialq.popleft()
+				arduino.write(msg)
+				print "Serial queue length after append: ", len(serialq)
+				print "%s: %s--msg: %s" % ("serialRead", time.ctime(time.time()),msg )
+	
+	def serialRead(self, delay, arduino, ipq, btq):
+		stop_flag = 0
+		while stop_flag == 0:
+			#if arduino.read() !=None: #check for empty string/char when reading.
+			msg = arduino.read()
+			#append the msg to both bluetooth queue and ip queue
+			if(msg!=''):
+				btq.append(msg)
+				ipq.append(msg)
+				print "BT queue length after append: ", len(btq)
+				print "IP queue length after append: ", len(ipq)
+				print "%s: %s--msg: %s" % ("serialRead", time.ctime(time.time()),msg )
+			time.sleep(delay)
 
 	def startServices(self):
 		ready1=[False]
 		ready2=[False]
+		ready3=[False]
 		thread.start_new_thread(self.android.startBTService, (1.0,ready1))
 		thread.start_new_thread(self.pc.startIPService, (1.0,ready2))
+		thread.start_new_thread(self.arduino.startSerialService, (1.0,ready3))
 		while True:
-			if ready1[0]!=True or ready2[0]!=True:
+			if ready1[0]!=True or ready2[0]!=True or ready3[0]!=True:
 				pass
 			else:
 				print "break off"
+				time.sleep(3)
 				break
 
 	def mainStart(self):
 		print "entering mainStart"
-		thread.start_new_thread (self.ipWrite, (0.5, self.pc, self.btq))
-		thread.start_new_thread (self.ipRead,  (0.5, self.pc, self.ipq))
-		thread.start_new_thread (self.btWrite, (0.5, self.android, self.ipq))
-		thread.start_new_thread (self.btRead,  (0.5, self.android, self.btq))
+		print "waiting for start command from android"
+		while(self.android.read()!='start'):
+			time.sleep(0.5)
+		print "start received...\nstarting communication:"
+
+		thread.start_new_thread (self.ipRead,  (0.5, self.pc, self.btq, self.serialq))
+		thread.start_new_thread (self.ipWrite, (0.5, self.pc, self.ipq))
+		thread.start_new_thread (self.btWrite, (0.5, self.android, self.btq))
+		#thread.start_new_thread (self.btRead,  (0.5, self.android, self.btq))
+		thread.start_new_thread (self.serialRead,  (0.5, self.arduino, self.ipq, self.btq))
+		thread.start_new_thread (self.serialWrite, (0.5, self.arduino, self.serialq))
+
 		#except:
 		while True:
 			time.sleep(4.0)
