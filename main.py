1a43a80ddb3665daf46a0302aa277b69c63d1113
asetiadi001 <asetiadi001@e.ntu.edu.sg>
2014-10-01 15:56:31 +0800

    Commit on 4 pm Oct1

--- main.py
+++ main.py
@@ -26,63 +26,65 @@ class Main:
 				print "writing to pc: ", msg
 				pc.write(msg)
 
-	def ipRead (self, delay, pc, ipq):
+	def ipRead (self, delay, pc, btq, serq):
 		stop_flag = 0
 		while stop_flag == 0:
 			msg = pc.read()
 			if(msg!=''):
-				ipq.append(msg)
-				print "IP queue length after append: ", len(ipq)
-
-				print "%s: %s--msg: %s" % ("ipRead", time.ctime(time.time()),msg )
+				btq.append(msg)
+				serq.append(msg)
+				#print "IP queue length after append: ", len(ipq)
+				print "append to btq and serq: ", msg
+				#print "%s: %s--msg: %s" % ("ipRead", time.ctime(time.time()),msg )
 			time.sleep (delay)
 
-	def btWrite (self, delay, android, serialq):
+	def btWrite (self, delay, android, btq):
 		
 		stop_flag = 0
 		while stop_flag == 0:
 			time.sleep (delay)
 			#print "btWrite awake - not"
-			if len(serialq) >0:
-				msg = serialq.popleft()
+			if len(btq) >0:
+				msg = btq.popleft()
 				print "Writing to android: ", msg
 				android.write(msg)
 				#print "Serial queue length after pop: " , len(serialq)
 
-	def btRead (self, delay, android, btq):
+	def btRead (self, delay, android, pcq, serq):
 		stop_flag = 0
 		while stop_flag == 0:
-			print 'btRead in blocking mode'
+			print 'btRead in blocking mode while waiting for android input...'
 			msg = android.read()
 			if(msg!=''):
 				#print "From android: %s" % (msg)
 				print "Append android queue: ", msg
-				btq.append(msg)
+				pcq.append(msg)
+				serq.append(msg)
 				#print "BT queue length after append: ", len(btq)
 				time.sleep (delay)
 
-	def serialWrite(self, delay, arduino, btq):
+	def serialWrite(self, delay, arduino, serq):
 		stop_flag = 0
 		while stop_flag == 0:
 			time.sleep(delay)
 			#print "serialWrite awake - not"
-			if len(btq) > 0:
-				msg = btq.popleft()
+			if len(serq) > 0:
+				msg = serq.popleft()
 				print "Writing to arduino: %s" %(msg)
 				arduino.write(msg)
 				#print "BT queue length after pop: ", len(btq)
 				#print "%s: %s--msg: %s" % ("serialRead", time.ctime(time.time()),msg )
 	
-	def serialRead(self, delay, arduino, serialq, ipq):
+	def serialRead(self, delay, arduino, btq, ipq):
 		stop_flag = 0
 		while stop_flag == 0:
 			print "serialRead in blocking mode"
 			#if arduino.read() !=None: #check for empty string/char when reading.
 			msg = arduino.read()
 			#append the msg to both bluetooth queue and ip queue
-			if re.match(r'[a-zA-Z0-9]',msg,re.I):
+			if re.match(r'[a-zA-Z0-9]+',msg,re.I):
 				print "Append to pc and arduino queue: ", msg
-				serialq.append(msg)
+				btq.append(msg)
 				ipq.append(msg)
 
 				#print "From arduino: ",msg
@@ -112,7 +114,7 @@ class Main:
 			time.sleep(0.5)
 		print "start received...\nstarting communication:"
 
-		#thread.start_new_thread (self.ipRead,  (0.5, self.pc, self.btq, self.serialq))
+		thread.start_new_thread (self.ipRead,  (0.5, self.pc, self.btq, self.serialq))
 		thread.start_new_thread (self.ipWrite, (0.5, self.pc, self.ipq))
 		thread.start_new_thread (self.btRead,  (0.5, self.android, self.btq))
 		thread.start_new_thread (self.btWrite, (0.5, self.android, self.serialq))
